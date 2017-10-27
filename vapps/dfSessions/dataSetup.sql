a8479ce9013e6bb6a92245797c1d07cae1f52d86
Vineet Sinha <vineet.sinha@salesforce.com>
2017-10-27 11:09:01 -0400

    basic support for a dream force sessions script

---
+++ vapps/dfSessions/dataSetup.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Sessions
+(
+  Level char(128),
+  Industry char(128),
+  Session_Theme char(128),
+  Role char(128),
+  Products char(128),
+  Room char(128),
+  Venue_Name char(128),
+  Session_Date char(128),
+  Session_Time char(128),
+  Session_Start_Time timestamp without time zone,
+  Attendance_Category char(128),
+  Session_Duration integer,
+  Enrollment_Count integer,
+  Session_Format char(128),
+  Session_Name char(128),
+  Session_Abstract char(2048)
+)
---
+++ vapps/dfSessions/testApi.js
@@ -0,0 +1,99 @@
+
+var violet = require('violet-conversations/lib/violet').script();
+var violetTime = require('violet-conversations/lib/violetTime')(violet);
+
+var violetStorePG = require('violet-conversations/lib/violetStorePG.js')(violet);
+
+
+// mock objects
+var response = violet._getResponseForDebugging({
+  getSession: ()=>{}
+}, {});
+
+// test prep
+violetStorePG.store.propOfInterest = {
+  'sessions': [
+    'level',
+    'industry',
+    'session_theme',
+    'role',
+    'products',
+    'room',
+    'venue_name',
+    'session_date',
+    'session_time',
+    'session_start_time',
+    'attendance_category',
+    'session_duration',
+    'enrollment_count',
+    'session_format',
+    'session_name',
+    'session_abstract'
+  ]
+}
+
+// test methods
+var loadAllSessions = () => {
+  setTimeout(()=>{
+    response.load({objName:'sessions', queryXtra: 'limit 5'})
+      .then((records)=>{
+        console.log('load results:', records);
+        console.log('loaded ' + records.length + ' records');
+      });
+  }, 2*1000);
+};
+var loadLeadershipSessions = () => {
+  setTimeout(()=>{
+    response.load({objName:'sessions', filter: "session_theme like '%Leadership%'", queryXtra: 'limit 3'})
+      .then((records)=>{
+        console.log('load results:', records);
+        console.log('loaded ' + records.length + ' records');
+      });
+  }, 2*1000);
+};
+
+// tgtIndustry = Agriculture, Automotive, Communications, Consumer Goods, Education, Energy, Financial Services, Government, Healthcare, Financial Services, High Tech, Hospitality/Travel, Life Sciences, Manufacturing, Media, Nonprofit, Professional Services, Retail, Transportation, Transportation & Logistics, Other
+var loadSessionsByIndustry = (tgtIndustry) => {
+  setTimeout(()=>{
+    response.load({objName:'sessions', filter: `industry like '%${tgtIndustry}%'`, queryXtra: 'limit 3'})
+      .then((records)=>{
+        console.log('load results:', records);
+        console.log('loaded ' + records.length + ' records');
+      });
+  }, 2*1000);
+};
+// tgtTheme = Admission/Recruiting, Admission/Recruiting, Advancement/Fundraising, Apps & Architecture, Channel Sales Management, Customer Support, E-commerce, Email, Emerging Small Business, Emerging Trends, Enterprise, Equality, Forecasting, Futures Lab, Gamification, GDPR, Innovation, Integration, Intelligence, Internet of Things, IT Help Desk, Leadership, Lightning Adoption, Marketing Automation, Mindfulness, Mobile, Privacy, Retail, Sales Enablement, Sales Productivity, Salesforce Careers, Salesforce on Salesforce, Salesforce Philanthropy, Security, Selling with Partners, Small & Medium Business, Social Marketing, Student Success/Community Engagement, Sustainability, Thought Leadership, Tips & Tricks, Trailhead Learning, User Experience, User Interface, Women in Leadership, Women in Technology
+var loadSessionsByTheme = (tgtTheme) => {
+  setTimeout(()=>{
+    response.load({objName:'sessions', filter: `session_theme like '%${tgtTheme}%'`, queryXtra: 'limit 3'})
+      .then((records)=>{
+        console.log('load results:', records);
+        console.log('loaded ' + records.length + ' records');
+      });
+  }, 2*1000);
+};
+
+// tgtRole = Architect, Consultant, Developer, E-Commerce, Entrepreneur, Executive, Finance & Accounting, Human Resources, IT, Marketing, Merchandising, New Customer, Operations, Other, Partner, Consulting, Partner, ISV, Product Management, Purchasing & Procurement, Sales, Salesforce Administrator, Service & Support, Technical Architect
+var loadSessionsByRole = (tgtRole) => {
+  setTimeout(()=>{
+    response.load({objName:'sessions', filter: `role like '%${tgtRole}%'`, queryXtra: 'limit 3'})
+      .then((records)=>{
+        console.log('load results:', records);
+        console.log('loaded ' + records.length + ' records');
+      });
+  }, 2*1000);
+};
+
+// tgtProduct = Analytics Cloud, App Cloud, AppExchange, Chatter, Cloud Services, Commerce Cloud, Community Cloud, Data.com, Desk.com, Einstein, Einstein Analytics, Einstein Discovery, Financial Services Cloud, Force.com, Health Cloud, Heroku, IoT Cloud, Lightning, Marketing Cloud, Pardot, Platform, Quip, Sales Cloud, Salesforce Billing, Salesforce CPQ, Salesforce DMP, Salesforce Inbox, Salesforce Platform, Salesforce Quote-to-Cash, Salesforce1 Mobile, Service Cloud
+var loadSessionsByProduct = (tgtProduct) => {
+  setTimeout(()=>{
+    response.load({objName:'sessions', filter: `products like '%${tgtProduct}%'`, queryXtra: 'limit 3'})
+      .then((records)=>{
+        console.log('load results:', records);
+        console.log('loaded ' + records.length + ' records');
+      });
+  }, 2*1000);
+};
+
+// loadAllSessions();
+// loadLeadershipSessions();
