8291bc4952b91433357fb182ac79315fc19aaa75
Vineet Sinha <vineet.sinha@gmail.com>
2018-08-08 17:55:18 -0400

    no need for the alexa route since we support google now aat the same time

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var violetSrvr = require('violet/lib/violetSrvr')('/alexa');
+var violetSrvr = require('violet/lib/violetSrvr')();
 violetSrvr.listAppsAt('/');
 var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
 
