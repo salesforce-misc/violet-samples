3f37dc475c59bff9a9a26ef974f7925c8844a270
Vineet Sinha <vineet.sinha@gmail.com>
2019-08-15 09:53:42 -0400

    comment changes - to make future testing easier

--- apps/todo/testApi.js
+++ apps/todo/testApi.js
@@ -3,6 +3,7 @@
 
 var quipSvc = new require('./svc.js');
 var utils = new require('./utils.js');
+const fs = require('fs');
 
 
 // dig into starred folders
@@ -14,7 +15,29 @@ var utils = new require('./utils.js');
 var mainDoc = process.env.QUIP_TGT_DOC_ID;
 
 // dig into particular document with checklist
-// quipSvc.getThread(mainDoc);
+quipSvc.getThread(mainDoc);
+
+// dig into messages for a particular document
+// quipSvc.getClient().getMessages({threadId: mainDoc}, (err, resp)=>{
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(resp)
+//   }
+// });
+
+// add message for a particular document
+// quipSvc.getClient().newMessage({
+//   threadId: mainDoc,
+//   content: 'https://quip.com/eTIAEAG12St - can we afford to do this?',
+//   silent: true
+// }, (err, resp)=>{
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(resp)
+//   }
+// });
 
 // add items to thread after section
 // quipSvc.appendItems(mainDoc, ['BBB - 10', 'BBB - 20']);
@@ -31,6 +54,21 @@ var mainDoc = process.env.QUIP_TGT_DOC_ID;
 // quipSvc.modifyListItem(mainDoc, 'TddACAurP6C', ['<del>The test item</del>']);
 
 // list items
-quipSvc.getItems(mainDoc, /*asList*/false, (err, items)=>{
-  console.log(JSON.stringify(items, null, 2));
-});
+// quipSvc.getItems(mainDoc, /*asList*/false, (err, items)=>{
+//   console.log(JSON.stringify(items, null, 2));
+// });
+
+// quipSvc.appendImage(mainDoc, 'image001.png', fs.readFileSync(__dirname + '/image001.png'));
+
+// create doc and write id
+// quipSvc.getClient().newDocument({
+//   title: 'Amazing Test',
+//   format: 'markdown',
+//   content: 'From the VR Environment\n\nOne of the things to keep in mind is\n\n that machines are not intelligent'
+// }, (err, resp)=>{
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(`Doc created with id:${resp.thread.id} and url: ${resp.thread.link}`);
+// });
